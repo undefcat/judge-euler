4b7726e6f79a239eeff974e1bd59e59c56156130
undefcat <undefcat@gmail.com>
2019-02-07 10:42:45 +0900

    update gen.sieve

--- src/gen/sieve.go
+++ src/gen/sieve.go
@@ -1,25 +1,73 @@
 package gen
 
-import "math"
+import (
+	"math"
+)
 
-func Sieve(n int) []bool {
-	isPrime := make([]bool, n+1)
-	isPrime[0] = true
-	for i := 1; i < len(isPrime); i *= 2 {
-		copy(isPrime[i:], isPrime[:i])
+type Sieve interface {
+	IsPrime(n int) bool
+}
+
+func NewSieve(n int) Sieve {
+	return NewBitSieve(n)
+}
+
+type boolSieve []bool
+
+func (it boolSieve) IsPrime(n int) bool {
+	return it[n]
+}
+
+func NewBoolSieve(n int) Sieve {
+	sieve := make(boolSieve, n+1)
+	sieve[0] = true
+	for i := 1; i < len(sieve); i *= 2 {
+		copy(sieve[i:], sieve[:i])
 	}
 
-	isPrime[0], isPrime[1] = false, false
+	sieve[0], sieve[1] = false, false
 
 	sqrtN := int(math.Sqrt(float64(n)))
 
 	for i := 2; i <= sqrtN; i++ {
-		if isPrime[i] {
+		if sieve[i] {
 			for j := i*i; j <= n; j += i {
-				isPrime[j] = false
+				sieve[j] = false
 			}
 		}
 	}
 
-	return isPrime
+	return sieve
+}
+
+type bitSieve []uint64
+
+func NewBitSieve(n int) Sieve {
+	sieve := make(bitSieve, n/64+1)
+	sieve[0] = 1<<64-1
+	for i := 1; i < len(sieve); i *= 2 {
+		copy(sieve[i:], sieve[:i])
+	}
+
+	sieve.setComposite(0)
+	sieve.setComposite(1)
+	sqrtN := int(math.Sqrt(float64(n)))
+
+	for i := 2; i <= sqrtN; i++ {
+		if sieve.IsPrime(i) {
+			for j := i*i; j <= n; j += i {
+				sieve.setComposite(j)
+			}
+		}
+	}
+
+	return sieve
+}
+
+func (it bitSieve) setComposite(n int) {
+	it[n>>6] &^= 1 << (uint(n)&63)
+}
+
+func (it bitSieve) IsPrime(n int) bool {
+	return (it[n>>6] & (1 << (uint(n)&63))) > 0
 }
